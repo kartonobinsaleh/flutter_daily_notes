8eeb8b6b58392460aef1ec8b1962338e946d9368
kartonobinsaleh <kartono.saleh@gmail.com>
2024-10-04 15:54:55 +0700

    Create LoginPage layout with custom widgets integration

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_todo_list/constants/colors.dart';
+import 'package:flutter_todo_list/widgets/custom_account_text.dart';
+import 'package:flutter_todo_list/widgets/custom_button.dart';
+import 'package:flutter_todo_list/widgets/custom_ilustration.dart';
+import 'package:flutter_todo_list/widgets/custom_textfield.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -35,113 +38,33 @@ class _LoginPageState extends State<LoginPage> {
           child: Column(
             children: [
               const SizedBox(height: 20),
-              ilustration(),
+              const CustomIlustration(
+                imagePath: 'images/schedule_meeting.png',
+                height: 300,
+              ),
               const SizedBox(height: 50),
-              textfield(email, _focusNode1, "Email", Icons.email),
+              CustomTextfield(
+                controller: email,
+                focusNode: _focusNode1,
+                hintText: "Email",
+                icon: Icons.email,
+              ),
               const SizedBox(height: 10),
-              textfield(password, _focusNode2, "Password", Icons.password),
+              CustomTextfield(
+                controller: password,
+                focusNode: _focusNode2,
+                hintText: "Password",
+                icon: Icons.password,
+              ),
               const SizedBox(height: 8),
-              account(),
+              CustomAccountText(
+                questionText: "Don't have an account?",
+                actionText: "Sign Up",
+                onActionTap: () {},
+              ),
               const SizedBox(height: 20),
-              button(),
+              const CustomButton(text: "Login"),
             ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget account() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Text(
-            "Don't have a account?",
-            style: TextStyle(
-              color: Colors.grey[700],
-              fontSize: 14,
-            ),
-          ),
-          const SizedBox(width: 5),
-          Text(
-            "Sign Up",
-            style: TextStyle(color: primaryColor, fontSize: 14, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(width: 2),
-        ],
-      ),
-    );
-  }
-
-  Widget button() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: Container(
-        alignment: Alignment.center,
-        width: double.infinity,
-        height: 50,
-        decoration: BoxDecoration(
-          color: primaryColor,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: const Text(
-          "Login",
-          style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
-  }
-
-  Widget textfield(TextEditingController controller, FocusNode focusNode, String hintText, IconData icon) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: TextField(
-          controller: controller,
-          focusNode: focusNode,
-          style: const TextStyle(fontSize: 18, color: Colors.black),
-          decoration: InputDecoration(
-              prefixIcon: Icon(
-                icon,
-                color: focusNode.hasFocus ? primaryColor : const Color(0xffc5c5c5),
-              ),
-              contentPadding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-              hintText: hintText,
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: const BorderSide(
-                  color: Color(0xffc5c5c5),
-                  width: 2.0,
-                ),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide(
-                  color: primaryColor,
-                  width: 2.0,
-                ),
-              )),
-        ),
-      ),
-    );
-  }
-
-  Widget ilustration() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: Container(
-        width: double.infinity,
-        height: 300,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/schedule_meeting.png'),
-            fit: BoxFit.cover,
           ),
         ),
       ),
