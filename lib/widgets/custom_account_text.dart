9bc8d7fe598a425661a4bdeca7daec019fe5a481
kartonobinsaleh <kartono.saleh@gmail.com>
2024-10-04 15:52:54 +0700

    Add CustomAccountText widget for account-related navigation text

---
+++ lib/widgets/custom_account_text.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_list/constants/colors.dart';
+
+class CustomAccountText extends StatelessWidget {
+  final String questionText;
+  final String actionText;
+  final VoidCallback onActionTap;
+
+  const CustomAccountText({
+    required this.questionText,
+    required this.actionText,
+    required this.onActionTap,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            questionText,
+            style: TextStyle(
+              color: Colors.grey[700],
+              fontSize: 14,
+            ),
+          ),
+          const SizedBox(width: 5),
+          GestureDetector(
+            onTap: onActionTap,
+            child: Text(
+              actionText,
+              style: TextStyle(
+                color: primaryColor,
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
