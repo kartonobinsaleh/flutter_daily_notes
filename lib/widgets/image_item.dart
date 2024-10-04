5fe10429b6c5d7ccda60202ed1fd97b604a0292d
kartonobinsaleh <kartono.saleh@gmail.com>
2024-10-04 22:20:51 +0700

    Create ImageItem widget for add_note_page

---
+++ lib/widgets/image_item.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_list/constants/colors.dart';
+
+class ImageItem extends StatelessWidget {
+  final int index;
+  final bool isSelected;
+  final Function() onTap;
+
+  const ImageItem({
+    required this.index,
+    required this.isSelected,
+    required this.onTap,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 140,
+        margin: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            width: 2,
+            color: isSelected ? primaryColor : const Color(0xffc5c5c5),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'images/$index.png',
+              fit: BoxFit.cover,
+              height: 100,
+              width: double.infinity,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
