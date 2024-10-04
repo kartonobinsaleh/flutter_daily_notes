368d19ff7aa27d05a063b13b8b843e8d30247a62
kartonobinsaleh <kartono.saleh@gmail.com>
2024-10-04 22:38:02 +0700

    Implement scroll-based visibility toggle for FloatingActionButton on HomePage

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_todo_list/constants/colors.dart';
+import 'package:flutter_todo_list/screens/add_note_page.dart';
+import 'package:flutter_todo_list/widgets/task_item.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +12,57 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool show = true;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: NotificationListener<UserScrollNotification>(
+          onNotification: (notification) {
+            if (notification.direction == ScrollDirection.forward) {
+              setState(
+                () {
+                  show = true;
+                },
+              );
+            }
+            if (notification.direction == ScrollDirection.reverse) {
+              setState(
+                () {
+                  show = false;
+                },
+              );
+            }
+            return true;
+          },
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return TaskItem();
+            },
+            itemCount: 10,
+          ),
+        ),
+      ),
+      floatingActionButton: Visibility(
+        visible: show,
+        child: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => const AddNotePage(),
+              ),
+            );
+          },
+          backgroundColor: primaryColor,
+          shape: const CircleBorder(),
+          child: const Icon(
+            Icons.add,
+            size: 32,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }
