706bfcb5b23ebe05df61f9964fb35a6116cf8223
kartonobinsaleh <kartono.saleh@gmail.com>
2024-10-06 02:31:52 +0700

    update note model

---
+++ lib/data/models/note.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Note {
+  final int id;
+  final String title;
+  final String description;
+  final DateTime? date;
+  final int image;
+
+  Note({
+    this.id = 0,
+    this.title = "",
+    this.description = "",
+    this.date,
+    this.image = 0,
+  });
+
+  Note copyWith({
+    int? id,
+    String? title,
+    String? description,
+    DateTime? date,
+    TimeOfDay? time,
+    int? image,
+  }) {
+    return Note(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      date: date ?? this.date,
+      image: image ?? this.image,
+    );
+  }
+
+  factory Note.fromJson(Map<String, dynamic> json) {
+    return Note(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      date: json['date'] != null ? DateTime.parse(json['date']) : null,
+      image: json['image'] ?? '1',
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'date': date?.toIso8601String(),
+      'image': image,
+    };
+  }
+}
